e97eb41b58b02dac3baf7da65a765e634786ba0f
vanshika81 <106854576+vanshika81@users.noreply.github.com>
2023-03-08 19:44:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,179 @@
+let weather = {
+    apiKey: "f1ef083469f775e03b27db4a7c885273",
+    fetchWeather: function (city) { 
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+         "&units=metric&appid=" +
+          this.apiKey
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No weather found.");
+            throw new Error("No weather found.");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const { speed } = data.wind;
+      document.querySelector(".cityName").innerText = "Weather in " + name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = temp + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humidity: " + humidity + "%";
+      document.querySelector(".wind").innerText =
+        "Wind speed: " + speed + " km/h";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+    
+};
+
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search(),weather.cityName();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+  
+  weather.fetchWeather("Bangalore");
+
+
+
+
+function GetInfo() {
+
+    var newName = document.getElementById("cityInput");
+    var cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--"+newName.value+"--";
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=32ba0bfed592484379e51106cef3f204')
+.then(response => response.json())
+.then(data => {
+
+    //Getting the min and max values for each day
+    for(i = 0; i<6; i++){
+        document.getElementById("day" + (i+1) + "Min").innerHTML = "Min: " + Number(data.list[i].main.temp_min - 273.15).toFixed(1)+ "°C";
+        //Number(1.3450001).toFixed(2); // 1.35
+    }
+
+    for(i = 0; i<6; i++){
+        document.getElementById("day" + (i+1) + "Max").innerHTML = "Max: " + Number(data.list[i].main.temp_max - 273.15).toFixed(2) + "°C";
+    }
+
+    // Weather Icons
+     for(i = 0; i<6; i++){
+        document.getElementById("img" + (i+1)).src = "https://openweathermap.org/img/wn/"+
+        data.list[i].weather[0].icon
+        +".png";
+    }
+    console.log(data)
+
+})
+
+.catch(err => alert("Confirm"))
+}
+
+function DefaultScreen(){
+    document.getElementById("cityInput").defaultValue = "Bangalore";
+    GetInfo();
+}
+
+
+var d = new Date();
+var weekday = ["Sunday", "  Monday  ", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+
+function CheckDay(day){
+    if(day + d.getDay() > 6){
+        return day + d.getDay() - 7;
+    }
+    else{
+        return day + d.getDay();
+    }
+}
+
+    for(i = 0; i<6; i++){
+        document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
+    }
+
+
+
+document.getElementById("day1Min").innerHTML = Math.round(data.list[0].main.temp_min - 273.15, -2);
+document.getElementById("day2Min").innerHTML = Math.round(data.list[1].main.temp_min - 273.15, -2);
+document.getElementById("day3Min").innerHTML = Math.round(data.list[2].main.temp_min - 273.15, -2);
+document.getElementById("day4Min").innerHTML = Math.round(data.list[3].main.temp_min - 273.15, -2);
+document.getElementById("day5Min").innerHTML = Math.round(data.list[4].main.temp_min - 273.15, -2);
+document.getElementById("day6Min").innerHTML = Math.round(data.list[5].main.temp_min - 273.15, -2);
+
+document.getElementById("day1Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day2Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day3Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day4Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day5Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+document.getElementById("day6Max").innerHTML = Math.round(data.list[0].main.temp_max - 273.15, -2);
+
+document.getElementById("img1").src = "http://openweathermap.org/img/w/"+
+data.list[0].weather[0].icon
++".png";
+document.getElementById("img2").src = "http://openweathermap.org/img/w/"+
+data.list[1].weather[0].icon
++".png";
+document.getElementById("img3").src = "http://openweathermap.org/img/w/"+
+data.list[2].weather[0].icon
++".png";
+document.getElementById("img4").src = "http://openweathermap.org/img/w/"+
+data.list[3].weather[0].icon
++".png";
+document.getElementById("img5").src = "http://openweathermap.org/img/w/"+
+data.list[4].weather[0].icon
++".png";
+document.getElementById("img6").src = "http://openweathermap.org/img/w/"+
+data.list[5].weather[0].icon
++".png";
+
+
+document.getElementById("day1").innerHTML = weekday[CheckDay(0)];
+document.getElementById("day2").innerHTML = weekday[CheckDay(1)];
+document.getElementById("day3").innerHTML = weekday[CheckDay(2)];
+document.getElementById("day4").innerHTML = weekday[CheckDay(3)];
+document.getElementById("day5").innerHTML = weekday[CheckDay(4)];
+document.getElementById("day6").innerHTML = weekday[CheckDay(5)];
+
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.cityName();
+  });
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
